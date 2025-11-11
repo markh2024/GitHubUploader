3dcbd57d3879f4fae047600943523f331c3dac9e
markh2024 <159157948+markh2024@users.noreply.github.com>
2025-11-11 00:20:00 +0000

    initial commit

---
+++ build/CMakeFiles/GitHubUploader.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitHubUploader.dir/src/GitHubUploader.cpp.o"
+  "CMakeFiles/GitHubUploader.dir/src/GitHubUploader.cpp.o.d"
+  "CMakeFiles/GitHubUploader.dir/src/main.cpp.o"
+  "CMakeFiles/GitHubUploader.dir/src/main.cpp.o.d"
+  "dist/GitHubUploader"
+  "dist/GitHubUploader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GitHubUploader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
